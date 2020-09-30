9cceb750d91378fd909d68c20f22313b47e17e2b
David Cohan <davidcohan@me.ar>
2020-09-30 13:48:31 -0300

    some scripts here and there

--- logstash/increase_backoff.sh
+++ logstash/increase_backoff.sh
@@ -4,5 +4,5 @@ ENV=$1
 pushd ansible
 export ANSIBLE_HOST_KEY_CHECKING=False
 INVENTORY=$(jarvis ec2 find -st running -n logstash-worker -an $ENV --ansible)
-ansible-playbook -i $INVENTORY restart_service.yml --become -K
+ansible-playbook -i $INVENTORY increase_backoff.yml --become -K
 popd
