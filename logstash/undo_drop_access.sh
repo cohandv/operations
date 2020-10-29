fded0c9b53e9c7b685ddd4d2d11300521333d8e7
Jonathan Lowenstern <jonigl@gmail.com>
2020-10-29 18:24:02 -0300

    adding undo_drop_access and drop_access_list

---
+++ logstash/undo_drop_access.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+ENV=$1
+
+pushd ansible
+export ANSIBLE_HOST_KEY_CHECKING=False
+INVENTORY=$(jarvis ec2 find -st running -n logstash-worker -a $ENV --ansible)
+ansible-playbook -i $INVENTORY undo_drop_access.yml --become -K
+popd
