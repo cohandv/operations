0a003d4a96301c1920522cc876e95b14e46a8480
David Cohan <davidcohan@me.ar>
2020-11-01 10:42:44 -0300

    improved segmentation on account, serialized the task, so we can restart all automatically

--- logstash/drop_access.sh
+++ logstash/drop_access.sh
@@ -3,6 +3,6 @@ ENV=$1
 
 pushd ansible
 export ANSIBLE_HOST_KEY_CHECKING=False
-INVENTORY=$(jarvis ec2 find -st running -n logstash-worker -an $ENV --ansible)
+INVENTORY=$(jarvis ec2 find -st running -n logstash-worker -a $ENV --ansible)
 ansible-playbook -i $INVENTORY drop_access.yml --become -K
 popd
