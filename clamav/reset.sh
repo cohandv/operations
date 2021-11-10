5b542249d0eb4bd69d15fd4f37a9afaa7cd35eea
El Deivi <davidcohan@me>
2021-11-10 16:28:10 -0300

    little improvement here and there

--- clamav/reset.sh
+++ clamav/reset.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 TERRAIN=$1
-read -s -p "Enter SSH Password: " ANSIBLE_PASSWORD
-echo checking instances on $TERRAIN
+echo Checking instances on $TERRAIN
 export ANSIBLE_HOST_KEY_CHECKING=False
+INVENTORY=""
 for account in $(jarvis account find -f data.terrain=$TERRAIN -o id -h)
 do
   echo Processing $account
-  INVENTORY=$(jarvis ec2 find -st running -a $account --ansible|grep -v found)
-  if [[ -z $INVENTORY ]]; then
-    echo "No running instances found for $account"
-    continue
-  else
-    ansible-playbook -i $INVENTORY ./ansible/reset_clamav.yml --become --extra-vars "ansible_sudo_pass=$ANSIBLE_PASSWORD"
-  fi
+  INVENTORY="$INVENTORY,$(jarvis ec2 find -st running -a $account --ansible|grep -v found)"
 done
+
+if [[ -z INVENTORY ]]; then
+  echo "No running instances found for $account"
+  continue
+else
+  echo Running ansible on the inventory
+  read -s -p "Enter SSH Password: " ANSIBLE_PASSWORD
+  ansible-playbook -i $INVENTORY ./ansible/reset_clamav.yml --become --extra-vars "ansible_sudo_pass=$ANSIBLE_PASSWORD"
+fi
 
